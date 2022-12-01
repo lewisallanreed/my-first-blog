d6e4933790eb1828caa1bde894529059cc3a297d
lewisallanreed <psychedelicorgan@gmail.com>
2022-12-01 19:08:52 +0100

    My Django Girls app, first commit

---
+++ TestPY.py
@@ -0,0 +1,9 @@
+def hi(name):
+    print('Hi '+name+'!')
+
+girls = ['Mary', 'Kate', 'Anna Lee']
+for i in range(0,3):
+    hi(girls[i])
+    print('Next girl')
+
+#https://tutorial.djangogirls.org/en/django_admin/ stopped at this tutorial URL
